080a9d2ab98064e3901a919595936a3e40c53375
Netanel Gilad <netanelgilad@gmail.com>
2019-01-08 08:27:31 +0200

    pass S12.5_A12_T{1,2,3}

--- packages/prophet-tests/test262.spec.ts
+++ packages/prophet-tests/test262.spec.ts
@@ -7,7 +7,8 @@ import { sync as rimrafSync } from "rimraf";
 const testFilesGlobs = [
   `language/statements/if/S12.5_A1.{1,2}_T{1,2}.js`,
   `language/statements/if/S12.5_A10_T{1,2}.js`,
-  `language/statements/if/S12.5_A11.js`
+  `language/statements/if/S12.5_A11.js`,
+  `language/statements/if/S12.5_A12_T{1,2,3}.js`
 ];
 
 const testRoot = join(dirname(require.resolve("test262/package.json")), "test");
