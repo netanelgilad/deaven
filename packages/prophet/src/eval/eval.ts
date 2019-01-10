dba34fcca151b849f15c670a5372d7bdb7fc4765
Netanel Gilad <netanelgilad@gmail.com>
2019-01-10 08:35:32 +0200

    implement S12.5_A2

--- packages/prophet/src/eval/eval.ts
+++ packages/prophet/src/eval/eval.ts
@@ -6,6 +6,7 @@ import { TESString } from "../string/String";
 import { unsafeCast } from "../unsafeGet";
 import { parse } from "@babel/parser";
 import { evaluate } from "../evaluate";
+import { isExpressionStatement } from "@babel/types";
 
 export const evalFn = ESFunction(function*(
   _self: Any,
@@ -16,8 +17,19 @@ export const evalFn = ESFunction(function*(
   const parsedSource = parse(source);
   const statements = parsedSource.program.body;
 
-  return statements.reduce(
-    ([, prevExecContext], statement) => evaluate(statement, prevExecContext),
-    tuple(Undefined as Any, execContext)
-  );
+  let currentEvaluationResult = tuple(Undefined, execContext);
+  for (const statement of statements.slice(0, statements.length - 1)) {
+    currentEvaluationResult = yield evaluate(
+      statement,
+      currentEvaluationResult[1]
+    );
+  }
+
+  const lastStatement = statements[statements.length - 1];
+
+  if (isExpressionStatement(lastStatement)) {
+    return evaluate(lastStatement.expression, currentEvaluationResult[1]);
+  }
+
+  return evaluate(lastStatement, currentEvaluationResult[1]);
 });
