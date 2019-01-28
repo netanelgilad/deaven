1762b1fd5fec77a53e001d821d6abfffd730663e
Netanel Gilad <netanelgilad@gmail.com>
2019-01-28 15:46:46 +0200

    initial implementation of jest-runner-typecheck

---
+++ external-types/create-jest-runner/index.d.ts
@@ -0,0 +1,2 @@
+export declare function pass(options: unknown): unknown;
+export declare function fail(options: unknown): unknown;
---
+++ packages/jest-runner-typecheck/src/index.ts
@@ -0,0 +1,3 @@
+module.exports = require("create-jest-runner").createJestRunner(
+  require.resolve("./run")
+);
